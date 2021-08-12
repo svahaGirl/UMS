d496bf54bf551d4e34f881cdb00e2ad0f09f017e
svahaGirl <fyanton@hotmail.com>
2021-08-12 12:11:32 -0700

    add backend and frontend projects

---
+++ UMSWebParent/UMSBackEnd/src/main/java/org/perscholas/umsbackend.admin/MainController.java
@@ -0,0 +1,16 @@
+package org.perscholas.umsbackend.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+
+
+    }
+
+}
