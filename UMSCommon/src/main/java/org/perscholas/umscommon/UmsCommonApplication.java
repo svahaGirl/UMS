bf6f63d6cc24ce1b89184d57fc091b49d470889b
svahaGirl <fyanton@hotmail.com>
2021-08-11 21:36:23 -0700

    Initial commit

---
+++ UMSCommon/src/main/java/org/perscholas/umscommon/UmsCommonApplication.java
@@ -0,0 +1,13 @@
+package org.perscholas.umscommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmsCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmsCommonApplication.class, args);
+    }
+
+}
---
+++ UMSWebParent/src/test/java/org/perscholas/umswebparent/UmsWebParentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perscholas.umswebparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmsWebParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
