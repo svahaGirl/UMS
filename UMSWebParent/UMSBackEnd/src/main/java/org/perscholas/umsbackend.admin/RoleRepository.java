b84957abc627a3ec523ce387c2060b06e0db2752
Ying - a buddhist developer <fyanton@hotmail.com>
2021-08-13 20:08:44 -0700

    Create RoleRepository,java
    Create RoleRepository.java

---
+++ UMSWebParent/UMSBackEnd/src/main/java/org/perscholas/umsbackend.admin/RoleRepository.java
@@ -0,0 +1,11 @@
+package org.perscholas.umsbackend.admin.user;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shop.common.entity.Role;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Integer>{
+
+}
