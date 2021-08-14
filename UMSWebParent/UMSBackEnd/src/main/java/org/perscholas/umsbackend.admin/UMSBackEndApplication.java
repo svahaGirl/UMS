a6609e15b88891e12e14e0ca46cc0b9a039f0ed6
Ying - a buddhist developer <fyanton@hotmail.com>
2021-08-13 20:09:22 -0700

    Update UMSBackEndApplication.java

--- UMSWebParent/UMSBackEnd/src/main/java/org/perscholas/umsbackend.admin/UMSBackEndApplication.java
+++ UMSWebParent/UMSBackEnd/src/main/java/org/perscholas/umsbackend.admin/UMSBackEndApplication.java
@@ -1,4 +1,4 @@
-package com.shop.admin;
+package package org.perscholas.umsbackend.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
