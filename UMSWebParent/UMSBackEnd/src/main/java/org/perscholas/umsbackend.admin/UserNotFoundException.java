b39981d984c3e6a68c0e8564034871519dc50bd1
Ying - a buddhist developer <fyanton@hotmail.com>
2021-08-16 12:14:59 -0700

    Create UserNotFoundException.java
    Create UserNotFoundException.java

---
+++ UMSWebParent/UMSBackEnd/src/main/java/org/perscholas/umsbackend.admin/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package org.perscholas.umsbackend.admin.user;
+
+public class UserNotFoundException extends Exception {
+
+	public UserNotFoundException(String message) {
+		super(message);
+		
+	}
+
+}
